e51d29aa38c2b6582638a0fafba1b5c47e24f50c
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-08-23 22:09:35 +0330

    first commit

--- seo/urls.py
+++ seo/urls.py
@@ -1,1 +1,14 @@
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'seo', views.SEOPageView, basename='seo')
+router.register(r'keyword', views.KeywordView, basename='keyword')
+
 app_name = 'seo'
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
