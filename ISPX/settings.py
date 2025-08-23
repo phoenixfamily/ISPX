3c4a261d2b98052cb039be5f4f58369fe98c3f76
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-08-23 12:35:43 +0330

    first commit

--- ISPX/settings.py
+++ ISPX/settings.py
@@ -120,14 +120,15 @@ WSGI_APPLICATION = 'ISPX.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv("DB_HOST", "localhost"),
-        'PORT': os.getenv("DB_PORT", "5432"),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
