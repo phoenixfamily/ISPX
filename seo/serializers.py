23bd08e00e5e2946ce4f2b9852391db6be6ffbfc
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-08-23 23:00:41 +0330

    first commit

--- seo/serializers.py
+++ seo/serializers.py
@@ -3,13 +3,22 @@ from rest_framework import serializers
 from seo.models import SEOPage, Keyword
 
 
-class SEOPageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SEOPage
-        fields = ['page_url', 'title', 'description', 'keywords']
-
-
 class KeyWordSerializer(serializers.ModelSerializer):
     class Meta:
         model = Keyword
-        fields = ['name']
+        fields = ["id", "name_en", "name_fa"]   # فیلدهای واقعی DB که modeltranslation ساخته
+
+class SEOPageSerializer(serializers.ModelSerializer):
+    # اگر بخواهی فقط id کیوردها را بگیری/بفرستی:
+    keyword_ids = serializers.PrimaryKeyRelatedField(
+        source="keywords", many=True, queryset=Keyword.objects.all(), write_only=True, required=False
+    )
+
+    class Meta:
+        model = SEOPage
+        fields = [
+            "page_url",
+            "title_en", "title_fa",
+            "description_en", "description_fa",
+            "keyword_ids",
+        ]
